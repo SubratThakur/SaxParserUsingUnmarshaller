fd2a69187d518a40e0abe4f0b4e713a16de12351
thaksu05 <Subrat.Thakur.consultant@nielsen.com>
2017-04-17 20:52:27 +0530

    SAX Parser done using JAXB

--- SAXParserForUKEmployee/src\main\java/controller/TestClass.java
+++ SAXParserForUKEmployee/src\main\java/controller/TestClass.java
@@ -1,29 +1,17 @@
 package controller;	
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-
-import entity.DPSretrieveResponse;
+import entity.UKRequest.Request;
 import service.RequestUnMarshaller;
 
 public class TestClass {
-	@SuppressWarnings("unchecked")
 	public static void main(String argv[]) {
 		try{
-			File fXmlFile = new File("jigs.xml");
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
-			RequestUnMarshaller marsha=new RequestUnMarshaller(DPSretrieveResponse.class);
-			DPSretrieveResponse result=(DPSretrieveResponse)marsha.getRequestObjectFromXML(fXmlFile);
-			System.out.println(result.getXmlns());
+			File fXmlFile = new File("original.xml");
+			RequestUnMarshaller<Request> marshal=new RequestUnMarshaller<Request>(Request.class);
+			Request result=(Request)marshal.getRequestObjectFromXML(fXmlFile);
+			System.out.println(result.toString());
 		} catch (Exception e) {
 		e.printStackTrace();
 	    }
